a4acbf4e5f7d42757c1f8249a3f9a888c2d7f42f
Afterburner <rockey1024@icloud.com>
2019-03-31 14:25:31 +0800

    Moved to vs2013

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,33 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace LINQ
 {
-    class Player
+    internal class Player
     {
-        public string Name { get; }
-        public string TeamName { get; }
-        public int X { get; }
-        public int Y { get; }
+        private readonly string _name;
 
         public Player(string name, string teamName, int x, int y)
         {
-            this.Name = name;
-            this.TeamName = teamName;
-            this.X = x;
-            this.Y = y;
+            _name = name;
+            TeamName = teamName;
+            X = x;
+            Y = y;
         }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public string TeamName { get; private set; }
+        public int X { get; private set; }
+        public int Y { get; private set; }
     }
-    class Program
+
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             var players = new List<Player>
             {
                 new Player("Johe", "Honer", 100, 75), new Player("Micheal", "Sick", 65, 40),
-                new Player("Kanna", "Honer", 40, 80), new Player("Light", "Guru", 60, 30),
+                new Player("Kanna", "Honer", 40, 80), new Player("Light", "Guru", 60, 30)
             };
 
             // Anything that implements IEnumerable can be used as input to LINQ as well as foreach-loop, including array, List ...
@@ -35,24 +41,18 @@ namespace LINQ
             var in_TeamHoner = from p in players
                 let teamName = p.TeamName
                 where teamName == "Honer"
-                      orderby p.X descending 
+                orderby p.X descending
                 select new {p.Name, teamName = p.TeamName.ToLower()};
 
             // First time to invoke LINQ
-            foreach (var honerMember in in_TeamHoner)
-            {
-                Console.WriteLine(honerMember);
-            }
+            foreach (var honerMember in in_TeamHoner) Console.WriteLine(honerMember);
 
             // Change the state of the input to the LINQ, which is `players`
             players.Add(new Player("Nathan", "Honer", 1000, 1000));
             // And I query again
             Console.WriteLine("\nAfter inserting a new member to team Honer");
-            foreach (var honerMember in in_TeamHoner)
-            {
-                Console.WriteLine(honerMember);
-            }
-
+            foreach (var honerMember in in_TeamHoner) Console.WriteLine(honerMember);
+            
             Console.ReadLine();
         }
     }
